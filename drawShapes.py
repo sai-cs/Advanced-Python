5801d4f2786c7af4568606cc3d4fd98ff34ac593
sai-cs <143716469+sai-cs@users.noreply.github.com>
2024-11-11 18:46:10 -0500

    Add files via upload

---
+++ drawShapes.py
@@ -0,0 +1,45 @@
+from turtle import *
+
+# Test Arrow
+# for i in range(10):
+#  pendown()
+#  forward(15)
+#  penup()
+#  forward(15)
+
+# Square
+# for i in range(4):
+#     pendown()
+#     forward(100)
+#     right(90)
+
+# Triangle
+# for i in range(3):
+#     pendown()
+#     forward(100)
+#     right(120)
+
+# Shape with 6 sides
+# for i in range(6):
+#     pendown()
+#     forward(100)
+#     right(60)
+
+# Circle
+# for i in range(360):
+#     pendown()
+#     forward(1)
+#     right(1)
+
+# Star
+# for i in range(5):
+#     pendown()
+#     forward(100)
+#     right(144)
+
+# Triangular Spiral Illusion Graphic
+# for i in range(100):
+#     forward(10+5*i)
+#     right(120)
+
+
